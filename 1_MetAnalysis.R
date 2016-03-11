e6a378a7d1e639dbf3d470ef630882e7bb30ccea
crollinson <crollinson@gmail.com>
2016-03-11 16:44:04 -0500

    Blank met script to initialize repository

---
+++ 1_MetAnalysis.R
@@ -0,0 +1,4 @@
+# Assessing the scales and magnitude of climate change variability over the past millennium in the MIP drivers
+
+# 1. Stastical detection of significant change
+# 2. Comparison with paleoclimate reconstructions
