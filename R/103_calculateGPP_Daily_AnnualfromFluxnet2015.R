1d93c62df9d6fdb49f4defc83bf86957fe743e89
David Moore <davidjpmoore@email.arizona.edu>
2016-06-22 14:12:28 -0700

    Comparing GPP methods for Harvard in the Fluxnet release. Next action - extend for Howland and Sylvania

--- R/103_calculateGPP_Daily_AnnualfromFluxnet2015.R
+++ R/103_calculateGPP_Daily_AnnualfromFluxnet2015.R
@@ -27,10 +27,42 @@ Ha1_daily=read.csv(paste0(filepath,"./FLX_US-Ha1_FLUXNET2015_FULLSET_DD_1991-201
 # Header for fluxnet 2015 FULL dataset
 FluxnetFULLheader = read.csv("D:/GlobalDatasets/FLUXNET06202016/FLUXNET2015_FULLSET_variable_list.csv")
 
-
-plot(Ha1_Monthly$GPP_NT_CUT_MEAN)
-plot(Ha1_annual$GPP_DT_VUT_50)
-
+  
+  #Night time method mean, 5% and 95% vs reference
+  
+  plot(Ha1_Monthly$GPP_NT_VUT_REF, Ha1_Monthly$GPP_NT_VUT_MEAN, xlim=c(-5,20),xlab="GPP_NT_VUT_REF", ylim=c(-5,20),ylab="Monthly GPP_NT_VUT_Mean+_95%", col=1) 
+  par(new=T)
+  plot(Ha1_Monthly$GPP_NT_VUT_REF, Ha1_Monthly$GPP_NT_VUT_95, xlim=c(-5,20),xlab="GPP_NT_VUT_REF", ylim=c(-5,20),ylab="Monthly GPP_NT_VUT_Mean+_95%", col=34) 
+  par(new=T)
+  plot(Ha1_Monthly$GPP_NT_VUT_REF, Ha1_Monthly$GPP_NT_VUT_05, xlim=c(-5,20),xlab="GPP_NT_VUT_REF", ylim=c(-5,20),ylab="Monthly GPP_NT_VUT_Mean+_95%", col=30) 
+  
+  
+  
+  plot(Ha1_annual$GPP_NT_VUT_REF, Ha1_annual$GPP_NT_VUT_MEAN, xlim=c(0,2500),xlab="GPP_NT_VUT_REF", ylim=c(0,2500),ylab="Annual GPP_NT_VUT_Mean+_95%", col=1) 
+  par(new=T)
+  plot(Ha1_annual$GPP_NT_VUT_REF, Ha1_annual$GPP_NT_VUT_95, xlim=c(0,2500),xlab="GPP_NT_VUT_REF", ylim=c(0,2500),ylab="Annual GPP_NT_VUT_Mean+_95%", col=34) 
+  par(new=T)
+  plot(Ha1_annual$GPP_NT_VUT_REF, Ha1_annual$GPP_NT_VUT_05, xlim=c(0,2500),xlab="GPP_NT_VUT_REF", ylim=c(0,2500),ylab="Annual GPP_NT_VUT_Mean+_95%", col=30) 
+  
+  #Night time method mean, 5% and 95% vs DAYTIME reference
+  
+  plot(Ha1_Monthly$GPP_DT_VUT_REF, Ha1_Monthly$GPP_NT_VUT_MEAN, xlim=c(-5,20),xlab="GPP_DT_VUT_REF", ylim=c(-5,20),ylab="Monthly GPP_NT_VUT_Mean+_95%", col=1) 
+  par(new=T)
+  plot(Ha1_Monthly$GPP_DT_VUT_REF, Ha1_Monthly$GPP_NT_VUT_95, xlim=c(-5,20),xlab="GPP_DT_VUT_REF", ylim=c(-5,20),ylab="Monthly GPP_NT_VUT_Mean+_95%", col=34) 
+  par(new=T)
+  plot(Ha1_Monthly$GPP_DT_VUT_REF, Ha1_Monthly$GPP_NT_VUT_05, xlim=c(-5,20),xlab="GPP_DT_VUT_REF", ylim=c(-5,20),ylab="Monthly GPP_NT_VUT_Mean+_95%", col=30) 
+  
+  
+  plot(Ha1_annual$GPP_DT_VUT_REF, Ha1_annual$GPP_NT_VUT_MEAN, xlim=c(0,2500),xlab="GPP_NT_VUT_REF", ylim=c(0,2500),ylab="Annual GPP_NT_VUT_Mean+_95%", col=1) 
+  par(new=T)
+  plot(Ha1_annual$GPP_DT_VUT_REF, Ha1_annual$GPP_NT_VUT_95, xlim=c(0,2500),xlab="GPP_NT_VUT_REF", ylim=c(0,2500),ylab="Annual GPP_NT_VUT_Mean+_95%", col=34) 
+  par(new=T)
+  plot(Ha1_annual$GPP_DT_VUT_REF, Ha1_annual$GPP_NT_VUT_05, xlim=c(0,2500),xlab="GPP_NT_VUT_REF", ylim=c(0,2500),ylab="Annual GPP_NT_VUT_Mean+_95%", col=30) 
+  
+  
+  #extend for Howland and Sylvania
+  
+  
 getwd()
 
 
