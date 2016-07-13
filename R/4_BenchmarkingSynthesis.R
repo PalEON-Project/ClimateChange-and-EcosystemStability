c12da8430d279aaffc521439f1b92a2255fad0ad
crollinson <crollinson@gmail.com>
2016-07-13 18:30:55 -0400

    Benchmarking: STEPPS conversion to model PFTs

--- R/4_BenchmarkingSynthesis.R
+++ R/4_BenchmarkingSynthesis.R
@@ -76,6 +76,15 @@ raw.dir <- "raw_data/Benchmarks/"
 
 if(!dir.exists(out.dir)) dir.create(out.dir)
 if(!dir.exists(fig.dir)) dir.create(fig.dir)
+
+hips <- data.frame(Site = c( "PHA",  "PHO",  "PUN",  "PBL",  "PDL",  "PMB"),
+                   lat  = c( 42.54,  45.25,  46.22,  46.28,  47.17,  43.61),
+                   lon  = c(-72.18, -68.73, -89.53, -94.58, -95.17, -82.83),
+                   latmin = c( 42.50,  45.00,  46.00,  46.00,  47.00,  43.50),
+                   latmax = c( 43.00,  45.50,  46.50,  46.50,  47.50,  44.00),
+                   lonmin = c(-72.50, -69.00, -90.00, -95.00, -95.50, -83.00),
+                   lonmax = c(-72.00, -68.50, -89.50, -94.50, -95.00, -82.50))
+
 # -------------------------------------------
 
 # -------------------------------------------
@@ -89,9 +98,81 @@ if(!dir.exists(fig.dir)) dir.create(fig.dir)
 
 # ------------------
 #    2.02. Composition -- STEPPS 1 & 2
+#     -- Finding the dominant PFT by model specifications
 # ------------------
 {
-  # 
+  library(car); library(reshape)
+  # Read in the model-STEPPS crosswalk
+  pft.xwalk <- read.csv("raw_data/STEPPS_conversion.csv")
+  pft.xwalk$taxon <- toupper(pft.xwalk$STEPPS)
+  pft.xwalk$taxon <- recode(pft.xwalk$taxon, "'OTHER CONIFER'='OTHER.CONIFER'; 'OTHER HARDWOOD'='OTHER.HARDWOOD'")
+  
+  # Reading in STEPPS 1
+  
+  # --------
+  # Loading STEPPS 2
+  # --------
+  stepps2 <- readRDS(file.path(raw.dir, "STEPPS2", "r_hips_v1.0.RDS"))
+  stepps2$taxon <- as.factor(stepps2$taxon)
+  summary(stepps2)
+  
+  unique(stepps2$taxon)
+  
+  # Translating the STEPPS data into model PFTS & storing in a list
+  stepps2 <- merge(stepps2, pft.xwalk)
+  
+  # Changing stepps time to a calendar year & subsetting just that in the modeling temporal extent
+  stepps2$year <- 2000-stepps2$time*100
+  stepps2 <- stepps2[stepps2$year>=850,]
+  
+  stepps2.pft <- list()
+  for(m in names(pft.xwalk)[2:(ncol(pft.xwalk)-1)]){
+    print(m)
+    if(m=="SiBCASA") next
+    stepps2$pft.tmp <- stepps2[,m]
+    
+    # Re-aggregate to the model-specific PFTs
+    fcomp1 <- aggregate(stepps2[,c("prop")], by=stepps2[,c("site", "year", "iter", m)], FUN=sum)
+    
+    stepps2.pft[[m]] <- merge(data.frame(year=unique(stepps2$year)), data.frame(site=unique(stepps2$site)))
+    # Go through each site & each iteration to find which is the dominant PFT on average
+    for(s in unique(fcomp1$site)){
+      # Finding out which PFT has the highest fcomp across time & iterations
+      pft.avg <- aggregate(fcomp1[fcomp1$site==s, "x"], by=list(fcomp1[fcomp1$site==s, m]), FUN=mean)
+      names(pft.avg) <- c("pft", "fcomp.avg")
+      pft.dom <- pft.avg$pft[which(pft.avg$fcomp.avg == max(pft.avg$fcomp.avg) )]
+      
+      # Record the dominant pft
+      stepps2.pft[[m]][stepps2.pft[[m]]$site==s,"pft"] <- pft.dom
+      
+      # creating an array to store the output 
+      # dat.temp <- array(dim=c(length(unique(stepps2$time)), length(unique(stepps2$iter))))
+      dat.temp <- fcomp1[fcomp1[,m]==pft.dom & fcomp1$site==s,c("x", "iter", "year")]
+      dat.temp$iter <- as.ordered(dat.temp$iter)
+      dat.temp$year <- as.ordered(dat.temp$year)
+      
+      dat.temp <- recast(dat.temp, year~iter, fun.aggregate=mean)
+      
+      stepps2.pft[[m]][stepps2.pft[[m]]$site==s,"mean"] <- apply(dat.temp, 1, mean)
+      stepps2.pft[[m]][stepps2.pft[[m]]$site==s,"upr"] <- apply(dat.temp, 1, quantile, 0.025)
+      stepps2.pft[[m]][stepps2.pft[[m]]$site==s,"lwr"] <- apply(dat.temp, 1, quantile, 0.975)
+      
+    }
+  }
+  
+  pdf(file.path(fig.dir, "STEPPS2_ModelPFTs.pdf"))
+  for(m in names(stepps2.pft)){
+    print(
+      ggplot(data=stepps2.pft[[m]]) +
+        geom_ribbon(aes(x=year, ymin=lwr, ymax=upr, fill=site), alpha=0.4) +
+        geom_line(aes(x=year, y=mean, color=site, linetype=pft)) +
+        ggtitle(m)
+    )
+  }
+  dev.off()
+  
+  save(stepps2.pft, file=file.path(out.dir, "STEPPS2_ModelPFTs.RData"))
+  # --------
 }
 # ------------------
 
@@ -109,9 +190,45 @@ if(!dir.exists(fig.dir)) dir.create(fig.dir)
 #    2.05. Biomass -- NBCD
 # ------------------
 {
-  library(raster); library(rgdal)
-  nbcd <- raster(file.path(raw.dir, "NBCD_countrywide_biomass_240m_raster", "NBCD_countrywide_biomass_mosaic.tif"))
-  nbcd <- spTransform(nbcd, CRS("+proj=longlat"))
+  library(raster); library(rgdal); library(sp)
+  
+  # Crop the NBCD raster to just paleon to make it easier to deal with
+  # nbcd <- raster(file.path(raw.dir, "NBCD_countrywide_biomass_240m_raster", "NBCD_countrywide_biomass_mosaic.tif"))
+  # paleon <- raster("~/Dropbox/PalEON_CR/env_regional/env_paleon/domain_mask/paleon_domain.nc")
+
+  # Transform the paleon domain to make life easier
+  # paleon2 <- projectRaster(paleon, crs=projection(nbcd), filename=file.path(raw.dir, "NBCD_countrywide_biomass_240m_raster", "paleon_reproject"), overwrite=T)
+
+  # Crop to the paleon domain
+  # nbcd2 <- crop(nbcd, paleon2, filename=file.path(raw.dir, "NBCD_countrywide_biomass_240m_raster", "NBCD_paleon_domain"), overwrite=T)
+  # plot(nbcd2)
+
+  # Transforming the cropped grid to latlon and from Mg/cell to MgC/Ha and saving it to work with
+  # nbcd3 <- projectRaster(nbcd2, crs=projection(paleon), filename=file.path(raw.dir, "NBCD_countrywide_biomass_240m_raster", "NBCD_paleon_latlon"), overwrite=T)
+  # nbcd3
+  
+  nbcd3 <- raster(file.path(raw.dir, "NBCD_countrywide_biomass_240m_raster", "NBCD_paleon_latlon"))
+  nbcd3
+  plot(nbcd3)
+  
+  hips.bm <- data.frame(Site=hips$Site)
+  # Extracting the NBCD values just in the 0.5 x 0.5-degree cell for each site
+  # Convert to MgC/Ha (from Mg Biomass per cell)
+  for(i in 1:nrow(hips)){
+    print(hips$Site[i])
+    site.ext <- extent(c(hips$lonmin[i], hips$lonmax[i], hips$latmin[i], hips$latmax[i]))
+    bm.site <- crop(nbcd3, extent(c(hips$lonmin[i], hips$lonmax[i], hips$latmin[i], hips$latmax[i])), filename=file.path(raw.dir, "NBCD_countrywide_biomass_240m_raster", paste0("NBCD_", hips$Site[i])), overwrite=T)
+
+    hips.bm[i,"nbcd.mean"] <- mean(getValues(bm.site)/5.76*0.5)     
+    hips.bm[i,"nbcd.sd"  ] <- sd(getValues(bm.site)/5.76*0.5)     
+    hips.bm[i,"nbcd.min" ] <- min(getValues(bm.site)/5.76*0.5)     
+    hips.bm[i,"nbcd.max" ] <- max(getValues(bm.site)/5.76*0.5)     
+           
+  } # extent=xmin, xmax, ymin, ymax
+  hips.bm
+  
+  write.csv(hips.bm, file.path(out.dir, "NBCD_sites_summary.csv"), row.names=F)
+
 }
 # ------------------
 
